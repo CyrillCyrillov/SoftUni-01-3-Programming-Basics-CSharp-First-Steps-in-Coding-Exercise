0bef7369859678ebafdd02b0f98c625546ed0638
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:51:34 +0200

    Add files via upload

---
+++ Task05_Birthday_party/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task05_Birthday_party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rentOfHall = double.Parse(Console.ReadLine());
+            double cakePrice = rentOfHall * 20 / 100;
+            double drinks = cakePrice - (cakePrice * 45 / 100);
+            double animator = rentOfHall / 3;
+            double sum = rentOfHall + cakePrice + drinks + animator;
+            Console.WriteLine(sum);
+        }
+    }
+}
