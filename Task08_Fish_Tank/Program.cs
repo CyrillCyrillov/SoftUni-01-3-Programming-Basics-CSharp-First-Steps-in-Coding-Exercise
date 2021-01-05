1f1447d2b3dbbe4e89c39a1f9b0ba91762c2e1c3
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:54:36 +0200

    Add files via upload

---
+++ Task08_Fish_Tank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task08_Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            double volume = (length * width * height) * 0.001;
+            double water = volume * (1 - percent * 0.01);
+            Console.WriteLine(water);
+        }
+    }
+}
