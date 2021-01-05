31e5732e2614bc00dc43c3002f87d01e7b4d3965
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:50:23 +0200

    Add files via upload

---
+++ Task04_Vacation_Books_List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task03_Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSuma = double.Parse(Console.ReadLine());
+            int mounts = int.Parse(Console.ReadLine());
+            double lybor = double.Parse(Console.ReadLine());
+            double suma = depositSuma + mounts * ((depositSuma * lybor / 100) / 12);
+            Console.WriteLine(suma);
+        }
+    }
+}
