8cd1de6831c69c409f42fbf7bc7af23518e33f1e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:52:48 +0200

    Add files via upload

---
+++ Task06_Charity_Campaign/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task06_Charity_Campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int confectioners = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+            double sumCakes = cakes * 45;
+            double sumWaffles = waffles * 5.80;
+            double sumPancakes = pancakes * 3.20;
+            double sumPerDay = (sumCakes + + sumWaffles + sumPancakes) * confectioners;
+            double sumAll = sumPerDay * days;
+            double sum = sumAll - (sumAll / 8);
+            Console.WriteLine(sum);
+        }
+    }
+}
