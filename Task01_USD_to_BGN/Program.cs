8b58f3f759f0dbd94a02d8a15b756410b0523541
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:44:33 +0200

    Add files via upload

---
+++ Task01_USD_to_BGN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task01_USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double fixing = 1.79549;
+            double bgn = usd * fixing;
+            Console.WriteLine(bgn);
+        }
+    }
+}
