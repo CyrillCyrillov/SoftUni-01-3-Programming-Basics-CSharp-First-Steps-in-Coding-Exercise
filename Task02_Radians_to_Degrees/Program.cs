1fbec515cb0e55047745ab594138095c97df76ca
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:45:33 +0200

    Add files via upload

---
+++ Task02_Radians_to_Degrees/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task02_Radians_to_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double formula = 180 / Math.PI;
+            double degrees = radians * formula;
+            Console.WriteLine(Math.Round(degrees));
+        }
+    }
+}
