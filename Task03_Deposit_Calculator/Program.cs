c5052d35c1ac0f46512dc85e3c1c05ce19e84928
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 21:48:10 +0200

    Add files via upload

---
+++ Task03_Deposit_Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task03_Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            double hours = pages / pagesPerHour / days;
+            Console.WriteLine(hours);
+        }
+    }
+}
